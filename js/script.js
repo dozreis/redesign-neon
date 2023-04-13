66564d335f42971384127f08753fb08fd5d6a60f
Allan dos Reis <dozreisdev@gmail.com>
2023-04-13 01:30:03 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+//menuDropdown
+const btnDropdown = document.querySelector('.js-btn-dropdown');
+const dropdown = document.querySelector('.js-dropdown');
+function openDropdown(event) {
+    event.preventDefault();
+    dropdown.classList.toggle('active');
+}
+btnDropdown.addEventListener('click', openDropdown);
+dropdown.addEventListener('mouseleave', openDropdown);
+//menuFixed
+const header = document.getElementById('js-header');
+function fixedMenu() {
+    if(window.pageYOffset > 80) {
+        header.classList.add('fixed-menu');
+    } else {
+        header.classList.remove('fixed-menu');
+    }
+}
+document.addEventListener('scroll', fixedMenu);
+//menuMobile
+const btnMenu = document.getElementById('js-btn-menu-mobile');
+const overlayMenu = document.querySelector('.js-overlay');
+function openMenuMobile () {
+    document.documentElement.classList.toggle('menu-opened');
+}
+btnMenu.addEventListener('click', openMenuMobile);
+overlayMenu.addEventListener('click', openMenuMobile);
